03130032b93e0903cb932114127a7d2a3cfabe9b
Saurabh Vishwakarma <saurabhvishwakarma745@gmail.com>
2021-11-20 01:49:18 +0530

    Some change in website and chatbot

--- js/js/custom.js
+++ js/js/custom.js
@@ -5,3 +5,13 @@ $(document).ready(function() {
 
 	$('.my-conv-form-wrapper').convform({selectInputStyle: 'disable'})
 });
+function funct() {
+    var txt;
+    var r = confirm("Your Appointment have been confirm\nDetailes will be send via your email and phone number");
+    if (r == true) {
+      txt = "Confirmed!!!";
+    } else {
+      txt = "You pressed Cancel!";
+    }
+    // document.getElementById("demo").innerHTML = txt;
+  }
